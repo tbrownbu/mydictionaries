d7db856bbad43ce83d21d606b859ff0830049fe8
Todd <Todd_brown1@baylor.edu>
2022-09-20 22:24:31 -0500

    Hw 9 commit

--- 5_datastore.py
+++ 5_datastore.py
@@ -53,7 +53,7 @@ datastore = { "medical":[
 }
 
 
-outfile = open('retail_space2.csv','w')
+outfile = open('retail_space.csv','w')
 outfile.write('room-number,use,sq-ft,price\n')
 
 for x in datastore['medical']:
